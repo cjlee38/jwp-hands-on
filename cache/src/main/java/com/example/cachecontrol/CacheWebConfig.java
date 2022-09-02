7d5a4cb3899c7692f0aa60fcbd47500f89ede0c1
cjlee38 <cjlee38@github.com>
2022-09-02 15:46:38 +0900

    feat: 0단계 cache control 적용

--- cache/src/main/java/com/example/cachecontrol/CacheWebConfig.java
+++ cache/src/main/java/com/example/cachecontrol/CacheWebConfig.java
@@ -1,13 +1,28 @@
 package com.example.cachecontrol;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CacheWebConfig implements WebMvcConfigurer {
 
+    private final CacheInterceptor interceptor;
+
+    public CacheWebConfig(CacheInterceptor interceptor) {
+        this.interceptor = interceptor;
+    }
+
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .setCacheControl(CacheControl.noCache());
     }
 }
